c2fd9a58c84f6cb90a38b964cb9a36af941480e1
AlexRamirezCalonge <alex.ramirez.calonge@gmail.com>
2017-09-18 20:49:47 +0200

    Empezar de nuevo xD

--- network.go
+++ network.go
@@ -1,117 +1,24 @@
 package d7024e
 
-import "net"
-import "bufio"
-import "encoding/json"
-import "time"
-import "fmt"
-
 type Network struct {
-}
-
-type MessageType int
-
-const (
-   PING MessageType = 1 + iota
-   FINDCONTACT
-   FINDDATA
-   STORE
-   ADDNODE
-   RESPONSE
-)
-type Message struct {
-    Source Contact
-    MessageType MessageType
-    Content string
 }
 
 func Listen(ip string, port int) {
 	// TODO
 }
 
-func SendPingMessage(srcContact Contact, destContact Contact) bool {
-  messageToSend := &Message{srcContact, PING, destContact.ID.String()}
-  conn, conErr := net.Dial("udp", destContact.Address)
-  //jsonToSend, _ := json.Marshal(messageToSend)
-  //conn.Write([]byte(jsonToSend))
-  fmt.Println(messageToSend)
-  //listen for reply
-  input := make(chan string, 1)
-
-
-  if(conErr==nil){
-    //fmt.Println("Text to send: ")
-    text, err := json.Marshal(messageToSend)
-
-    if (err != nil) {
-      fmt.Println("Error ")
-      fmt.Println(err)
-    }
-    //fmt.Println(time.Now().String() + "Message to send server: "+string(text))
-
-    // send to socket
-    fmt.Fprintf(conn, string(text) + "\n")
-  }
-  go getInput(input, conn)
-
-  for {
-  		select {
-    		case i := <-input:
-    			var message Message
-    			json.Unmarshal([]byte(i),&message)
-    			if(message.MessageType==RESPONSE){
-    				return true
-    			}
-    		case <-time.After(4000 * time.Millisecond):
-    			fmt.Println("timed out")
-  			  return false
-  		}
-	}
+func (network *Network) SendPingMessage(contact *Contact) {
+	// TODO
 }
 
-//func getInput(input chan string, destContact Contact, srcContact Contact, conn connection) {
-  func getInput(input chan string, conn net.Conn) {
-    //for {
-    JSONmessage, err := bufio.NewReader(conn).ReadString('\n')
-
-    fmt.Println("Msg rcvd: " + JSONmessage)
-    if err != nil {
-    }
-    input <- JSONmessage
-    //}
-}
-
-func SendFindContactMessage(srcContact Contact, destContact Contact, contactToFind Contact) []Contact {
-	messageToSend := &Message{srcContact, FINDCONTACT,contactToFind.ID.String()}
-	//fmt.Print("messageToSend to messageToSend server: "+messageToSend.Content )
-
-	conn, _ := net.Dial("udp", destContact.Address)
-	//	  fmt.Print("Text to send: ")
-	  text, err := json.Marshal(messageToSend)
-	  if err != nil {
-		fmt.Println("Error " )
-		fmt.Println(err)
-	}
-	  //fmt.Println("Message to send server: "+string(text))
-
-	  // send to socket
-	  fmt.Fprintf(conn, string(text) + "\n")
-	  // listen for reply
-	  JSONmessage, _ := bufio.NewReader(conn).ReadString('\n')
-	  var message Message
-	  json.Unmarshal([]byte(JSONmessage),&message)
-	  var contacts []Contact
-	  json.Unmarshal([]byte(message.Content),&contacts)
-	  /*for i := range contacts {
-	  	fmt.Println("Message from server " +string(i) +" : "+ contacts[i].ID.String())
-	  }*/
-	  return contacts
+func (network *Network) SendFindContactMessage(contact *Contact) {
+	// TODO
 }
 
 func (network *Network) SendFindDataMessage(hash string) {
-	// TODO Sprint2
+	// TODO
 }
 
 func (network *Network) SendStoreMessage(data []byte) {
-	// TODO Sprint2
+	// TODO
 }
--- routingtable_test.go
+++ routingtable_test.go
@@ -6,22 +6,17 @@ import (
 )
 
 func TestRoutingTable(t *testing.T) {
-	srcContact := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000")
-	rt := NewRoutingTable(srcContact)
+	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
 
 	rt.AddContact(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
-	rt.AddContact(NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8003"))
-	rt.AddContact(NewContact(NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8004"))
-	rt.AddContact(NewContact(NewKademliaID("1111111300000000000000000000000000000000"), "localhost:8005"))
-	rt.AddContact(NewContact(NewKademliaID("1111111400000000000000000000000000000000"), "localhost:8006"))
-	rt.AddContact(NewContact(NewKademliaID("2111111400000000000000000000000000000000"), "localhost:8007"))
+	rt.AddContact(NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
+	rt.AddContact(NewContact(NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8002"))
+	rt.AddContact(NewContact(NewKademliaID("1111111300000000000000000000000000000000"), "localhost:8002"))
+	rt.AddContact(NewContact(NewKademliaID("1111111400000000000000000000000000000000"), "localhost:8002"))
+	rt.AddContact(NewContact(NewKademliaID("2111111400000000000000000000000000000000"), "localhost:8002"))
 
 	contacts := rt.FindClosestContacts(NewKademliaID("2111111400000000000000000000000000000000"), 20)
 	for i := range contacts {
 		fmt.Println(contacts[i].String())
 	}
-	
-	kademlia:=NewKademlia(*rt,2,1)
-	go kademlia.ServerThread("8000")
-	fmt.Println(SendPingMessage(srcContact, srcContact))
 }
