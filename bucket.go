6f838bcff0f83bc1c3c0ecabef9b70c7babee1f2
Adrian Prada <pradastur@gmail.com>
2017-09-18 15:58:51 +0200

    Merge push request

---
+++ bucket.go
@@ -0,0 +1,57 @@
+package d7024e
+
+import (
+	"container/list"
+)
+
+type bucket struct {
+	list *list.List
+}
+
+func newBucket() *bucket {
+	bucket := &bucket{}
+	bucket.list = list.New()
+	return bucket
+}
+
+func (bucket *bucket) AddContact(contact Contact, sourceContact Contact) {
+	var element *list.Element
+	for e := bucket.list.Front(); e != nil; e = e.Next() {
+		nodeID := e.Value.(Contact).ID
+
+		if (contact).ID.Equals(nodeID) {
+			element = e
+		}
+	}
+
+	if element == nil {
+		if bucket.list.Len() < bucketSize {
+			bucket.list.PushFront(contact)
+		}else{ //Test if the oldest node still alive
+			//fmt.Println("bucket full")
+			if(!SendPingMessage(sourceContact,bucket.list.Back().Value.(Contact))){
+				//fmt.Println("oldest node is dead")
+				bucket.list.Remove(bucket.list.Back())
+				bucket.list.PushFront(contact)
+			}
+}
+	} else {
+		bucket.list.MoveToFront(element)
+	}
+}
+
+func (bucket *bucket) GetContactAndCalcDistance(target *KademliaID) []Contact {
+	var contacts []Contact
+
+	for elt := bucket.list.Front(); elt != nil; elt = elt.Next() {
+		contact := elt.Value.(Contact)
+		contact.CalcDistance(target)
+		contacts = append(contacts, contact)
+	}
+
+	return contacts
+}
+
+func (bucket *bucket) Len() int {
+	return bucket.list.Len()
+}
