69c15782dc814a6d903054a10d94c6d879082ee2
AlexRamirezCalonge <alex.ramirez.calonge@gmail.com>
2017-10-10 14:13:49 +0200

    Main Scenario

--- main.go
+++ main.go
@@ -13,12 +13,12 @@ func main() {
 	file := NewFile("/data", true, []byte(" "))
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Introduce the address of your first node, ej: [0000000000000000000000000000000000000000]")
+	fmt.Println("Introduce a valid address of your first node, ej: [0000000000000000000000000000000000000000]")
 	fmt.Print("-> ")
 	firstAddress, _ := reader.ReadString('\n')
 	firstAddress = strings.Replace(firstAddress, "\r\n", "", -1)
 	fmt.Println("Address introduced: " + firstAddress)
-	fmt.Println("Introduce the port of your first node, ej: [8000]")
+	fmt.Println("Introduce a valid port of your first node, ej: [8000]")
 	fmt.Print("-> ")
 	firstPort, _ := reader.ReadString('\n')
 	firstPort = strings.Replace(firstPort, "\r\n", "", -1)
@@ -50,25 +50,60 @@ func main() {
 			fmt.Println("join [contact Contact]: join to the network using the contact passed as parameter")
 			fmt.Println("pin [file File]: mark a file as important (can't be removed)")
 			fmt.Println("unpin [file File]: dismark the file as important (can be removed)")
-			fmt.Println("cat [string address]: print the content of the file given")
+			fmt.Println("cat [file File]: print the content of the file given")
 			fmt.Println("store [file File] [string address]: save a file in the node")
 			fmt.Println("exit: leave the simulation")
 		} else if strings.Compare("new", text) == 0 {
-			fmt.Println("Introduce a valid address of 40 numbers, example --> [0000000000000000000000000000000000000000]")
-			address, _ := reader.ReadString('\n')
-			address = strings.Replace(address, "\r\n", "", -1)
 
-			fmt.Println("Address introduced: " + address)
+			fmt.Println("How many nodes to deploy")
 
-			fmt.Println("Introduce a valid port of 4 numbers, example --> 8000")
-			port, _ := reader.ReadString('\n')
-			port = strings.Replace(port, "\r\n", "", -1)
+			nNode, _ := reader.ReadString('\n')
+			nNode = strings.Replace(nNode, "\r\n", "", -1)
+			nodeInt, _ := strconv.Atoi(nNode)
 
-			fmt.Println("Port introduced: " + port)
-			portInt, _ := strconv.Atoi(port)
+			for i := 0; i < nodeInt; i++ {
+				if i < 10 {
+					port := i
+					KademliaIDName := NewKademliaID("000000000000000000000000000000000000000" + strconv.Itoa(port))
+					portNext := i + 1
+					if i != 9 {
+						KademliaIDNameNext := NewKademliaID("000000000000000000000000000000000000000" + strconv.Itoa(portNext))
+						go nextNode(KademliaIDName, KademliaIDNameNext, port, portNext)
+					} else {
+						KademliaIDNameNext2 := NewKademliaID("00000000000000000000000000000000000000" + strconv.Itoa(portNext))
+						go nextNode(KademliaIDName, KademliaIDNameNext2, port, portNext)
+					}
+					//fmt.Println(KademliaIDName.String())
+					//KademliaIDnode++
+				} else {
+					port := i
+					KademliaIDName := NewKademliaID("00000000000000000000000000000000000000" + strconv.Itoa(port))
+					portNext := i + 1
+					KademliaIDNameNext := NewKademliaID("00000000000000000000000000000000000000" + strconv.Itoa(portNext))
 
-			go node(address, portInt)
+					//fmt.Println(KademliaIDName)
+					//KademliaIDnode++
+					go nextNode(KademliaIDName, KademliaIDNameNext, port, portNext)
+				}
+			}
+			go finalNode(nodeInt)
 
+			/*
+				fmt.Println("Introduce a valid address of 40 numbers, example --> [0000000000000000000000000000000000000000]")
+				address, _ := reader.ReadString('\n')
+				address = strings.Replace(address, "\r\n", "", -1)
+
+				fmt.Println("Address introduced: " + address)
+
+				fmt.Println("Introduce a valid port of 4 numbers, example --> 8000")
+				port, _ := reader.ReadString('\n')
+				port = strings.Replace(port, "\r\n", "", -1)
+
+				fmt.Println("Port introduced: " + port)
+				portInt, _ := strconv.Atoi(port)
+
+				go node(address, portInt)
+			*/
 		} else if strings.Compare("join", text) == 0 {
 			fmt.Println("Une el nodo en la red usando el contacto pasado por parametro")
 			//var contact Contact
@@ -85,10 +120,11 @@ func main() {
 			fmt.Println("Select which one you want to see, introducing the number associated")
 			numFile, _ := reader.ReadString('\n')
 			numFile = strings.Replace(numFile, "\r\n", "", -1)
-			fileWanted, _ := strconv.Atoi(numFile)
-			fmt.Println("Este es: " + string(fileWanted))
+			//	fileWanted, _ := strconv.Atoi(numFile)
+			fmt.Println("Este es: " + numFile)
+			numFileInt, _ := strconv.Atoi("" + numFile)
 			//no  hace el cat del file porque no ve que lo que introducimos es un numero
-			fileW := fileList[fileWanted]
+			fileW := fileList[numFileInt]
 			content := string(fileW.Content)
 			fmt.Println("Content is: " + content)
 		} else if strings.Compare("pin", text) == 0 {
@@ -162,6 +198,47 @@ func main() {
 		}
 
 	}
+}
+
+func nextNode(kademliaIDName *KademliaID, kademliaIDNameNext *KademliaID, port int, portNext int) {
+	mySelf := NewContact(kademliaIDName, "localhost:"+strconv.Itoa(8000+port))
+	next := NewContact(kademliaIDNameNext, "localhost:"+strconv.Itoa(8000+portNext))
+
+	rt := NewRoutingTable(mySelf)
+
+	rt.AddContact(mySelf)
+	rt.AddContact(next)
+
+	channel := make(chan []Contact)
+	kademlia := NewKademlia(*rt, 20, 3, channel)
+
+	go kademlia.network.Listen("localhost", 8000+port)
+}
+func finalNode(nodeInt int) {
+	var KademliaIDName *KademliaID
+	if nodeInt < 10 {
+		KademliaIDName = NewKademliaID("000000000000000000000000000000000000000" + strconv.Itoa(nodeInt))
+		if nodeInt != 9 {
+			KademliaIDName = NewKademliaID("000000000000000000000000000000000000000" + strconv.Itoa(nodeInt))
+		} else {
+			KademliaIDName = NewKademliaID("00000000000000000000000000000000000000" + strconv.Itoa(nodeInt))
+		}
+		//fmt.Println(KademliaIDName.String())
+		//KademliaIDnode++
+	} else {
+		KademliaIDName = NewKademliaID("00000000000000000000000000000000000000" + strconv.Itoa(nodeInt))
+	}
+
+	mySelf := NewContact(KademliaIDName, "localhost:"+strconv.Itoa(8000+nodeInt))
+
+	rt := NewRoutingTable(mySelf)
+
+	rt.AddContact(mySelf)
+
+	channel := make(chan []Contact)
+	kademlia := NewKademlia(*rt, 20, 3, channel)
+
+	go kademlia.network.Listen("localhost", 8099)
 }
 
 func node(address string, port int) {
